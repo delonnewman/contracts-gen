bacc75c2318c0f7f2198225a247b5a42d9599acb
Delon Newman <delnewman@salud.unm.edu>
2019-07-17 11:22:05 -0600

    added faker and gen/test as deps added Contracts::Keys

--- lib/contracts/gen.rb
+++ lib/contracts/gen.rb
@@ -1,2 +1,51 @@
 require "contracts/gen/version"
 require "contracts/builtin_ext"
+require 'gen/test'
+require 'faker'
+
+module Contracts
+  class Keys
+    def self.[](spec)
+      if spec[:required] and spec[:optional]
+        new(spec[:required], spec[:optional])
+      elsif spec[:required]
+        new(spec[:required])
+      elsif spec[:optional]
+        new(nil, spec[:optional])
+      else
+        raise "Invalid hash spec: #{spec.inspect}"
+      end
+    end
+
+    def initialize(required, optional)
+      @required = required
+      @optional = optional
+    end
+
+    def valid?(value)
+      if not value.respond_to?(:[])
+        false
+      else
+        @required.all? { |(k, v)| Contract.valid?(value[k], v) }
+      end
+    end
+
+    def generate
+      h = {}
+      unless @optional.nil? or @optional.empty?
+        @required.each do |(k, contract)|
+          h[k] = ::Gen.generate(contract)
+        end
+      end
+      unless @optional.nil? or @optional.empty?
+        n = Faker::Number.between(0, @optional.keys.length)
+        keys = @optional.keys.sample(n)
+        keys.each do |k|
+          contract = @optional[k]
+          h[k] = ::Gen.generate(contract)
+        end
+      end
+      h
+    end
+  end
+end
